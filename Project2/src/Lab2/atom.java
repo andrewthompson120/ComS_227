a442ce82d72bd2345f45d9eb7531293d0d0786d5
andrewthompson120 <andrewthompson120@gmail.com>
2017-01-27 11:56:54 -0600

    Finished Building atom.class
    built all of atom.class

--- Project2/src/Lab2/atom.java
+++ Project2/src/Lab2/atom.java
@@ -2,8 +2,31 @@ package Lab2;
 
 public class atom {
 	public atom(int givenProtons, int givenNeutrons, int givenElectrons){
+		 
+	}
+	
+	/*
+	 * Returns the atomic mass.
+	 */
+	 int getAtomicMass(int givenProtons, int givenNeutrons){ 
+		return givenProtons + givenNeutrons;
 		
+	}
+	 
+	/*
+	 * Returns the difference between the number of protons and electrons
+	 */
+	public int getAtomicCharge(int givenProtons, int givenElectrons){
 		
+		return givenProtons - givenElectrons;
 		
+	}
+	
+	/*
+	 * Decreases the number of protons and neutrons by 2
+	 */
+	void decay(int givenProtons, int givenNeutrons){
+		givenProtons = givenProtons - 2;
+		givenNeutrons = givenNeutrons - 2;
 	}
 }
