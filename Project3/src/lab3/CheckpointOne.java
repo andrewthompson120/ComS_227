c46084493cd390362b5a4bb539cca1d3e0e99525
andrewthompson120 <andrewthompson120@gmail.com>
2017-02-03 01:17:19 -0600

    Finished Checkpoint 1 Question 1

---
+++ Project3/src/lab3/CheckpointOne.java
@@ -0,0 +1,20 @@
+package lab3;
+
+public class CheckpointOne {
+	
+	public static void main(String[] argc) {
+		
+		int i;
+		int Answer = 0;		
+		for (i = 0; i < 1000000; i++) {			
+			Answer = i % 7;			
+		}
+		System.out.println("Checkpoint: 1, Question 1 Answer: " + Answer);
+		/*
+		 * Write a line of code that determines the one millionth number in this sequence: n % 7
+		 */
+		
+		
+	}
+	
+}
