59465faa5f0f18434f555dc88f2950b2c69fa27e
andrewthompson120 <andrewthompson120@gmail.com>
2017-02-03 06:35:35 -0600

    Finished Rabbit model

--- Project3/src/lab3/RabbitModel.java
+++ Project3/src/lab3/RabbitModel.java
@@ -3,20 +3,33 @@ package lab3;
 public class RabbitModel {
 	
 	private int rabbitPopulation; // Initial rabbit population
-	private int years; // Years to simulate
 	private int rabbitsBorn; // Rabbit birth rate per year
 	private int rabbitsDead; // Rabbits death rate per year
 	
 	public RabbitModel() {
+		
 	}
 	
 	public int getPopulation() {
-		rabbitPopulation = rabbitPopulation - rabbitsDead * years + rabbitsBorn * years;
 		return rabbitPopulation;
 	}
 	/*
-	 * Returns rabbit population at the end of the year and adjusts the population based on birth/death rates
+	 * Returns rabbit population
+	 * 	@return
 	 */
-	
+	 
+	public void simulateYear() {
+		rabbitPopulation = rabbitPopulation - rabbitsDead + rabbitsBorn;
+	}
+	/*
+	 * Simulates the passage of a year. 
+	 * Updates the number of rabbits alive at the end of the year
+	 */
+	public void reset() {
+		
+	}
+	/*
+	 * Resets the state of the model to the initial conditions
+	 */
 	
 }
