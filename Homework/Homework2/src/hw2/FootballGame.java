a37ebe63738ebe0ec188efe8cc570e848ef70e26
andrewthompson120 <andrewthompson120@gmail.com>
2017-02-11 06:56:58 -0600

    Finished hw2 skeleton code

---
+++ Homework/Homework2/src/hw2/FootballGame.java
@@ -0,0 +1,44 @@
+package hw2;
+
+/**
+ * This class encapsulates the logic and state for a simplified
+ * game of American football.  
+ * 
+ * @author YOUR NAME HERE
+ */
+public class FootballGame {
+  /**
+   * Number of points awarded for a touchdown.
+   */  
+  public static final int TOUCHDOWN_POINTS = 6;
+  
+  /**
+   * Number of points awarded for a successful extra point attempt
+   * after a touchdown.
+   */ 
+  public static final int EXTRA_POINTS = 1;
+  
+  /**
+   * Number of points awarded for a field goal.
+   */
+  public static final int FIELD_GOAL_POINTS = 3;
+  
+  /**
+   * Total length of the field from goal line to goal line, in yards.
+   */
+  public static final int FIELD_LENGTH = 100;
+  
+  /**
+   * Initial position of the offensive team after receiving a kickoff.
+   */ 
+  public static final int STARTING_POSITION = 70;
+  
+  /**
+   * Yards required to get a first down.
+   */
+  public static final int YARDS_FOR_FIRST_DOWN = 10;
+  
+  // TODO - everything else :)
+  
+
+}
