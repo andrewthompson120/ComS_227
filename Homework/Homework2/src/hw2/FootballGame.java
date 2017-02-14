567bcfcfdb23b024b10c904310bdcdf18354a5b0
andrewthompson120 <andrewthompson120@gmail.com>
2017-02-14 15:30:54 -0600

    FootballGame - Finished int methods

--- Homework/Homework2/src/hw2/FootballGame.java
+++ Homework/Homework2/src/hw2/FootballGame.java
@@ -44,6 +44,8 @@ public class FootballGame {
   private int teamBScore; // Holds score for team 1
   private int offenseTeam; // False: team 0; True: team 1;
   private int downNumber; // Holds the number for the down
+  private int yardsToFirstDown; // Yards until 1st down
+  private int yardsToGoalLine; // Yards to the end zone
   
   
   /**
@@ -54,7 +56,8 @@ public class FootballGame {
 	  teamBScore = 0;
 	  offenseTeam = 0;
 	  downNumber = 1;
-	  
+	  yardsToFirstDown = YARDS_FOR_FIRST_DOWN;
+	  yardsToGoalLine = STARTING_POSITION;
   }
   
   /**
@@ -71,6 +74,36 @@ public class FootballGame {
    */
   public int getOffense() {
 	  return offenseTeam;
+  }
+  
+  /**
+   * Returns the points for the indicated team.
+   * @param whichTeam
+   * @return teamAScore/teamBScore
+   */
+  public int getScore(int whichTeam) {
+	  if (whichTeam == 0) {
+		  return teamAScore;
+	  }
+	  else {
+		  return teamBScore;
+	  }
+  }
+  
+  /**
+   * Returns the number of yards the offense must advance the ball to get a first down.
+   * @return yardsToFirst
+   */
+  public int getYardsToFirstDown() {
+	  return yardsToFirstDown;
+  }
+  
+  /**
+   * Returns the location of the ball as the number of yards to the opposing team's goal line.
+   * @return yardsToGoalLine
+   */
+  public int getYardsToGoalLine() {
+	  return yardsToGoalLine;
   }
   
 
