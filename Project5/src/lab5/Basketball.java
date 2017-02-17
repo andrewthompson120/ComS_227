7f19884ae4a13513a98bbb7aea37fe26c6c63c30
andrewthompson120 <andrewthompson120@gmail.com>
2017-02-17 10:22:55 -0600

    Finished - TA Checkpt 1

--- Project5/src/lab5/Basketball.java
+++ Project5/src/lab5/Basketball.java
@@ -2,24 +2,28 @@ package lab5;
 
 public class Basketball {
 	
+	private boolean inflatedStatus;
+	private double diameter;
+	
   public Basketball(double givenDiameter) {
-  
+	  inflatedStatus = false;
+	  diameter = givenDiameter;
   }
 
   public boolean isDribbleable() {
-    return false;
+	  return inflatedStatus;
   }
 
   public double getDiameter() {
-    return 0;
+	  return diameter;
   }
 
   public double getCircumference() {
-    return 0;
+	  return 0;
   }
 
   public void inflate() {
-  
+	   inflatedStatus = true;
   }
   
 }
