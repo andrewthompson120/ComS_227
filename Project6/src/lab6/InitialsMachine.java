8a149867800b744b553898b3d86ef72ef104ff57
andrewthompson120 <andrewthompson120@gmail.com>
2017-03-02 00:35:48 -0600

    Init Commit

---
+++ Project6/src/lab6/InitialsMachine.java
@@ -0,0 +1,13 @@
+package lab6;
+
+public class InitialsMachine {
+	static public void main(String[] argc) {
+		
+		String currentName = "";
+		String initials = "";
+		
+		System.out.println("Enter name.");
+		
+		
+	}
+}
