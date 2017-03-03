7ee85c72962c22e42ece1bf2e05e611ccbe98c96
andrewthompson120 <andrewthompson120@gmail.com>
2017-03-03 01:15:27 -0600

    Finished checkpt1

--- Project6/src/lab6/InitialsMachine.java
+++ Project6/src/lab6/InitialsMachine.java
@@ -7,9 +7,14 @@ public class InitialsMachine {
 		
 		String nameIn = "dfsdf asdfas tTs Tdsv";
 		String initReturn = "";
+		int firstVowel = 0;
+		System.out.println("Name entered: " + nameIn);
 	
 		initReturn = getInitials(nameIn);
 		System.out.println(initReturn);
+		
+		firstVowel = getFirstVowel(nameIn);
+		System.out.println("First vowel at: " + firstVowel);
 		
 	}
 	
@@ -28,4 +33,57 @@ public class InitialsMachine {
 		return initials;
 		
 	}
+	
+	public static int getFirstVowel(String name) {
+		
+		int stringLength = name.length();
+		int firstIndex = -1;
+		
+		for (int i = 0; i < stringLength; i++) {
+			if(name.charAt(i) == 'a') {
+				firstIndex = name.indexOf('a');
+				break;
+			}
+			else if(name.charAt(i) == 'e') {
+				firstIndex = name.indexOf('e');
+				break;
+			}
+			else if(name.charAt(i) == 'i') {
+				firstIndex = name.indexOf('i');
+				break;
+			}
+			else if(name.charAt(i) == 'o') {
+				firstIndex = name.indexOf('o');
+				break;
+			}
+			else if(name.charAt(i) == 'u') {
+				firstIndex = name.indexOf('u');
+				break;
+			}
+			else if(name.charAt(i) == 'A') {
+				firstIndex = name.indexOf('A');
+				break;
+			}
+			else if(name.charAt(i) == 'E') {
+				firstIndex = name.indexOf('E');
+				break;
+			}
+			else if(name.charAt(i) == 'I') {
+				firstIndex = name.indexOf('I');
+				break;
+			}
+			else if(name.charAt(i) == 'O') {
+				firstIndex = name.indexOf('O');
+				break;
+			}
+			else if(name.charAt(i) == 'U') {
+				firstIndex = name.indexOf('U');
+				break;
+			}
+		}
+		
+		return firstIndex;
+		
+	}
+	
 }
