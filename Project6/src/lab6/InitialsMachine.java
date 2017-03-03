5587424cc06b02e39a708e66a70de17b4b784dfb
andrewthompson120 <andrewthompson120@gmail.com>
2017-03-03 00:44:35 -0600

    Finished Checkpt1 pt1

--- Project6/src/lab6/InitialsMachine.java
+++ Project6/src/lab6/InitialsMachine.java
@@ -1,13 +1,31 @@
 package lab6;
 
+import java.util.Scanner;
+
 public class InitialsMachine {
-	static public void main(String[] argc) {
+	public static void main(String[] argc) {
 		
-		String currentName = "";
+		String nameIn = "dfsdf asdfas tTs Tdsv";
+		String initReturn = "";
+	
+		initReturn = getInitials(nameIn);
+		System.out.println(initReturn);
+		
+	}
+	
+	public static String getInitials(String name) {
+		
 		String initials = "";
+		String currentString = "";
+		Scanner in = new Scanner(name);
 		
-		System.out.println("Enter name.");
+		while(in.hasNext()) {			
+			currentString = in.next();
+			initials = initials + currentString.charAt(0);
+		}
 		
+		in.close();
+		return initials;
 		
 	}
 }
